f238b30e6d876d44fe11e02a41e637163a2a11ad
Kjw19 <63536145+Kjw19@users.noreply.github.com>
2023-01-17 10:24:53 +0900

    FirstCommit

---
+++ ch01DI/src/main/java/kr/spring/ch01/SpringMain.java
@@ -0,0 +1,10 @@
+package kr.spring.ch01;
+
+public class SpringMain {
+
+	public static void main(String[] args) {
+		MessageBean messageBean = new MessageBean();
+		messageBean.sayHello("홍길동");
+		
+	}
+}
